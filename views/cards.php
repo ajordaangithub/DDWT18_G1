e1a717838e1c86e88c09bff170870bfd0d795ed1
Rolf <rolfdaling@gmail.com>
2018-12-19 13:44:20 +0100

    Changed some variable names and corrected the link.

--- views/cards.php
+++ views/cards.php
@@ -1,23 +1,24 @@
-<!-- Series count -->
+<!-- Rooms count -->
 <div class="card">
     <div class="card-header">
-        Series
+        Roomf
     </div>
     <div class="card-body">
         <p class="count">Available already has</p>
         <h2><?= $nbr_rooms ?></h2>
-        <p>series listed</p>
+        <p>rooms listed</p>
         <a href="/DDWT18/final/add/" class="btn btn-primary">List yours</a>
     </div>
 </div>
+<!-- Users count-->
 <div class="card">
     <div class="card-header">
         Users
     </div>
     <div class="card-body">
-        <p class="count">Series overview already has</p>
+        <p class="count">Rooms overview already has</p>
         <h2><?= $nbr_users ?></h2>
         <p>active users</p>
-        <a href="/DDWT18/week2/register/" class="btn btn-primary">Join now</a>
+        <a href="/DDWT18/final/register/" class="btn btn-primary">Join now</a>
     </div>
 </div>
