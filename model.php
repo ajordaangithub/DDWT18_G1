fc54bc750948d339796bbe87935f41560b51947d
nelisw <nielswesteneng@gmail.com>
2018-12-21 11:48:04 +0100

    Added Kamernet as title instead of Series Overview. (Can be found in getnavigation() function)

--- model.php
+++ model.php
@@ -100,7 +100,7 @@ function get_breadcrumbs($breadcrumbs) {
 function get_navigation($navigation_tpl, $active_id){
     $navigation_exp = '
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand">Series Overview</a>
+    <a class="navbar-brand">Kamernet</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
