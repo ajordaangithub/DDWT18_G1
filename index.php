38af5b7facb8bc401b86a78b1809b028c33db005
Rolf <rolfdaling@gmail.com>
2019-01-07 12:04:26 +0100

    Corrected typo in redirect of edit POST route. Now shows correct error message while updating.

--- index.php
+++ index.php
@@ -369,8 +369,7 @@ elseif (new_route('/DDWT18/final/edit/', 'post')) {
 
 
     /* Redirect to room GET route */
-    redirect(sprintf('/DDWT18/final/room/?room_id='.$room_id.'?error_msg=%s', json_encode($feedback)));
-    /* TODO: show error msg after updating */
+    redirect(sprintf('/DDWT18/final/room/?room_id='.$room_id.'&error_msg=%s', json_encode($feedback)));
 }
 
 /* Delete room POST */
